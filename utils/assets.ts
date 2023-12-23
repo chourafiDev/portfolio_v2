ab1a601e7c42dfc0cadb477ad55fe77e035130f6
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-23 22:10:55 +0100

    style: add star icon

--- utils/assets.ts
+++ utils/assets.ts
@@ -4,6 +4,7 @@ import icon1 from "/public/assets/imgs/icon1.svg";
 import icon2 from "/public/assets/imgs/icon2.png";
 import signature from "/public/assets/imgs/signature.svg";
 import logo from "/public/assets/imgs/logo.png";
+import star from "/public/assets/imgs/star.svg";
 import hand from "/public/assets/imgs/hand.svg";
 
-export { bg1, person4, icon1, icon2, signature, logo, hand };
+export { bg1, person4, icon1, icon2, signature, logo, hand, star };
