1b9aa66776d31c526763965a9753eb58136ad782
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-24 17:46:19 +0100

    styles: fix design

--- components/navbar/NavContact.tsx
+++ components/navbar/NavContact.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavContact = () => {
   return (
-    <Link href="/contact" className="btn btn-white bg-gradient">
+    <Link href="/contact" className="btn btn-white bg-gradient md:flex hidden">
       Let&apos;s talk
     </Link>
   );
