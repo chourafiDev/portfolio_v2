af77a05b7b99e9edfb45708c1c82b0b09ee30182
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-25 23:21:36 +0100

    styles: add computer image

--- utils/assets.ts
+++ utils/assets.ts
@@ -6,5 +6,6 @@ import signature from "/public/assets/imgs/signature.svg";
 import logo from "/public/assets/imgs/logo.png";
 import star from "/public/assets/imgs/star.svg";
 import hand from "/public/assets/imgs/hand.svg";
+import computer from "/public/assets/imgs/computer.svg";
 
-export { bg1, person4, icon1, icon2, signature, logo, hand, star };
+export { bg1, person4, icon1, icon2, signature, logo, hand, star, computer };
