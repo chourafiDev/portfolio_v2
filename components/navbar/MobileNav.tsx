a4fbfc3fc249a80051a862b08c5a41760ae5e5f4
chourafiDev <abdelmonaimechourafi@gmail.com>
2023-12-25 15:08:26 +0100

    refactor: clear code

--- components/navbar/MobileNav.tsx
+++ components/navbar/MobileNav.tsx
@@ -1,7 +1,5 @@
-import { usePathname } from "next/navigation";
-import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { menuSlide, scale, slide } from "@/utils/anim";
+import { menuSlide, slide } from "@/utils/anim";
 import Link from "next/link";
 import Image from "next/image";
 import { logo } from "@/utils/assets";
@@ -14,9 +12,6 @@ type ToggleNavProps = {
 };
 
 const MobileNav = ({ handleToggle }: ToggleNavProps) => {
-  const pathname = usePathname();
-  const [selectedIndicator, setSelectedIndicator] = useState(pathname);
-
   return (
     <motion.div
       variants={menuSlide}
@@ -25,12 +20,7 @@ const MobileNav = ({ handleToggle }: ToggleNavProps) => {
       exit="exit"
       className="h-screen bg-[#0c1f3b] w-[80%] fixed right-0 top-0 text-white z-50"
     >
-      <div
-        onMouseLeave={() => {
-          setSelectedIndicator(pathname);
-        }}
-        className="flex flex-col justify-between h-full px-10 py-6"
-      >
+      <div className="flex flex-col justify-between h-full px-10 py-6">
         <div className="flex justify-between items-center">
           <Link href="/" className="text-white">
             <Image
@@ -47,20 +37,12 @@ const MobileNav = ({ handleToggle }: ToggleNavProps) => {
           {navItems.map(({ title, href }, index) => (
             <motion.li
               key={index}
-              onMouseEnter={() => {
-                setSelectedIndicator(href);
-              }}
               custom={index}
               variants={slide}
               initial="initial"
               animate="enter"
               exit="exit"
             >
-              {/* <motion.div
-                  variants={scale}
-                  animate={isActive ? "open" : "closed"}
-                  className={styles.indicator}
-                ></motion.div> */}
               <Link href={href} className="text-lightest-slate/60">
                 <span className="text-green">0{index + 1}.</span> {title}
               </Link>
